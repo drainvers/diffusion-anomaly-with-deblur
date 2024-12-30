Added moving average to sampling, will generate fixed amount of samples before saving

guided_diffusion/image_datasets.py
@@ -1,5 +1,6 @@
 import math
 import random
+import os
 from pathlib import Path
 from PIL import Image
 import blobfile as bf
@@ -85,8 +86,8 @@ def _list_image_files_recursively(data_dir):
     results = []
     for entry in sorted(bf.listdir(data_dir)):
         full_path = bf.join(data_dir, entry)
-        ext = entry.split(".")[-1]
-        if "." in entry and ext.lower() in ["jpg", "jpeg", "png", "tiff", "tif", "npy"]:
+        ext = os.path.splitext(os.path.basename(full_path))[1]
+        if ext.lower() in [".jpg", ".jpeg", ".png", ".tiff", ".tif", ".npy"]:
             results.append(full_path)
         elif bf.isdir(full_path):
             results.extend(_list_image_files_recursively(full_path))
@@ -108,8 +109,6 @@ class ImageDataset(Dataset):
         super().__init__()
         self.resolution = resolution
         self.local_images = [p for ext in exts for p in Path(f'{image_paths}').glob(f'**/*.{ext}')]
-
-
         self.local_classes = None if classes is None else classes[shard:][::num_shards]
         self.random_crop = random_crop
         self.random_flip = random_flip
@@ -121,16 +120,16 @@ class ImageDataset(Dataset):
     def __getitem__(self, idx):
         path = self.local_images[idx]
         # name=str(path).split("/")[-1].split(".")[0]
-        name, ext = str(path).split("/")[-1].split(".")
+        name, ext = os.path.splitext(os.path.basename(path))
         print('path', name)
 
         # Readds ability to read known image formats, taken from upstream guided diffusion repo
-        if ext == 'npy':
+        if ext == '.npy':
             numpy_img = np.load(path)
         else:
-            numpy_img = np.asarray(Image.open(path).convert('L')) # Load in grayscale image as ndarray
-            numpy_img = cv2.resize(numpy_img, (256, 256), interpolation=cv2.INTER_AREA)
-            numpy_img = numpy_img[:, :, np.newaxis] # Changes image shape to (W, H, 1)
+            numpy_img = np.asarray(Image.open(path).convert('L')) # Use Pillow for TIF support
+            # numpy_img = cv2.resize(numpy_img, (256, 256), interpolation=cv2.INTER_AREA)
+            numpy_img = np.expand_dims(numpy_img, axis=2) # Changes image shape to (W, H, 1)
         arr = visualize(numpy_img).astype(np.float32)
 
         out_dict = {}
@@ -138,7 +137,7 @@ class ImageDataset(Dataset):
             out_dict["y"] = np.array(self.local_classes[idx], dtype=np.int64)
             out_dict["path"]=name
 
-        return np.transpose(arr, [2, 0, 1]), out_dict
+        return np.transpose(arr, [2, 0, 1]), out_dict # HWC -> CHW
 
 
 def center_crop_arr(pil_image, image_size):

scripts/classifier_sample_known.py
@@ -3,7 +3,7 @@ Generate a large batch of image samples from a model and save them as a large
 numpy array. This can be used to produce samples for FID evaluation.
 """
 import matplotlib.pyplot as plt
-import argparse
+import argparse, os
 from pathlib import Path
 from visdom import Visdom
 viz = Visdom(port=8850)
@@ -30,6 +30,7 @@ from guided_diffusion.script_util import (
 # Evaluation metrics
 from torcheval.metrics import PeakSignalNoiseRatio
 from torcheval.metrics import StructuralSimilarity
+from torcheval.metrics import FrechetInceptionDistance
 
 from sklearn.metrics import roc_auc_score
 from skimage.filters import threshold_otsu, threshold_mean
@@ -42,7 +43,12 @@ from torchvision.utils import draw_segmentation_masks, draw_bounding_boxes
 from torchvision.transforms.functional import to_pil_image
 from torchvision.ops import masks_to_boxes
 
-def smooth_segmentation(mask):
+def choose_sample_fn(diffusion_obj, use_ddim, use_ma_sampling):
+   if use_ddim:
+       if use_ma_sampling:
+           return
+       return diffusion_obj.ddim_sample_loop_known
+   
    pass
 
 def visualize(img):
@@ -58,7 +64,7 @@ def main():
     args = create_argparser().parse_args()
 
     dist_util.setup_dist()
-    logger.configure()
+    logger.configure(dir=args.result_dir)
 
     logger.log("creating model and diffusion...")
     model, diffusion = create_model_and_diffusion(
@@ -81,18 +87,13 @@ def main():
         batch_size=args.batch_size,
         image_size=args.image_size,
         class_cond=True,
+        deterministic=True
      )
      datal = iter(data)
    
     model.load_state_dict(
         dist_util.load_state_dict(args.model_path, map_location="cpu")
     )
-
-    # Load deblur checkpoint here
-    # For saving our generated images
-    result_dir = Path(f'results/{args.data_dir.split("/")[-1]}')
-    result_dir.mkdir(parents=True, exist_ok=True)
-    # End of deblur
 
     model.to(dist_util.dev())
     if args.use_fp16:
@@ -134,13 +135,19 @@ def main():
         return model(x, t, y if args.class_cond else None)
 
     logger.log("sampling...")
-    all_images = []
-    all_labels = []
+    all_orgs    = []
+    all_images  = []
+    all_labels  = []
+    all_names   = []
+    all_diffs   = []
 
-    for img in datal:
+    all_results = []
 
+    # for img in datal:
+    while len(all_images) * args.batch_size < args.num_samples:
         model_kwargs = {}
-     #   img = next(data)  # should return an image from the dataloader "data"
+        img = next(datal)
+        # img = next(data)  # should return an image from the dataloader "data"
         print('img', img[0].shape, img[1])
         if args.dataset=='brats':
             Labelmask = th.where(img[3] > 0, 1, 0)
@@ -210,78 +217,94 @@ def main():
             viz.image(colored_diff.transpose(2, 0, 1), opts=dict(caption=f'diff {img[1]["path"][0]}'))
 
             # Save image
-            heatmap_img = (colored_diff * 255).astype(np.uint8)
-            original_img = (np.concatenate((np.array(visualize(img[0][0, ...]).cpu()).transpose(1, 2, 0),) * 3, axis=-1) * 255).astype(np.uint8)
+            original_img = (np.concatenate((np.array(visualize(org[0, ...]).cpu()).transpose(1, 2, 0),) * 3, axis=-1) * 255).astype(np.uint8)
             sampled_img = (np.concatenate((np.array(visualize(sample[0, ...]).cpu()).transpose(1, 2, 0),) * 3, axis=-1) * 255).astype(np.uint8)
+            heatmap_img = (colored_diff * 255).astype(np.uint8)
 
-            thresh = threshold_otsu(visualize(diff))
-            logger.log(f'threshold: {thresh}')
-            mask = th.where(th.tensor(visualize(diff)) > 0.5, 1, 0)  #this is our predicted binary segmentation
-            viz.image(visualize(mask), opts=dict(caption=f'mask {img[1]["path"][0]}'))
+            # thresh = threshold_otsu(visualize(diff))
+            # logger.log(f'threshold: {thresh}')
+            # mask = th.where(th.tensor(visualize(diff)) > 0.5, 1, 0)  #this is our predicted binary segmentation
+            # viz.image(visualize(mask), opts=dict(caption=f'mask {img[1]["path"][0]}'))
 
             # Convert mask to boxes
-            obj_ids = th.unique(mask)
-            obj_ids = obj_ids[1:]
-            masks = mask == obj_ids[:, None, None]
+            # obj_ids = th.unique(mask)
+            # obj_ids = obj_ids[1:]
+            # masks = mask == obj_ids[:, None, None]
 
-            boxes = masks_to_boxes(masks)
-            drawn_boxes = draw_bounding_boxes((img[0][0, ...] * 255).type(th.uint8).repeat(3, 1, 1), boxes, colors="red")
+            # boxes = masks_to_boxes(masks)
+            # drawn_boxes = draw_bounding_boxes((img[0][0, ...] * 255).type(th.uint8).repeat(3, 1, 1), boxes, colors="red")
             # fig, _ = show(drawn_boxes)
-            viz.image(drawn_boxes, opts=dict(caption=f'bbox {img[1]["path"][0]}'))
+            # viz.image(drawn_boxes, opts=dict(caption=f'bbox {img[1]["path"][0]}'))
 
             # Image.fromarray((np.array(visualize(mask).cpu()) * 255).astype(np.uint8)).save(f'results/{args.data_dir.split("/")[-1]}/mask_{img[1]["path"][0]}.png')
 
             # Image.fromarray(heatmap_img).save(f'results/heatmap_{img[1]["path"][0]}.png')
             # Image.fromarray(sampled_img).save(f'results/sampled_{img[1]["path"][0]}.png')
 
-            mask = (np.array(mask.cpu().repeat(3, 1, 1)).transpose(1, 2, 0) * 255).astype(np.uint8)
-            drawn_boxes = np.array(drawn_boxes.cpu()).transpose(1, 2, 0).astype(np.uint8)
+            # mask = (np.array(mask.cpu().repeat(3, 1, 1)).transpose(1, 2, 0) * 255).astype(np.uint8)
+            # drawn_boxes = np.array(drawn_boxes.cpu()).transpose(1, 2, 0).astype(np.uint8)
 
-            print(original_img.shape)
-            print(sampled_img.shape)
-            print(heatmap_img.shape)
-            print(mask.shape)
-            print(drawn_boxes.shape)
-
-            result = Image.fromarray(np.hstack([original_img,
-                                                sampled_img,
-                                                heatmap_img,
-                                                mask,
-                                                drawn_boxes]))
-            result.save(f'results/{args.data_dir.split("/")[-2]}/{img[1]["path"][0]}.png')
+            result = np.hstack([original_img,
+                                sampled_img,
+                                heatmap_img])
+            all_results.append(result)
+            all_names.append(img[1]["path"][0])
 
             # End of save image
-
-            psnr.reset()
-            ssim.reset()
-
-            print(org.shape, sample.shape)
             psnr.update(org, sample)
             ssim.update(org, sample)
-
-            logger.log(f'{img[1]["path"][0]} psnr, ssim: {psnr.compute()}, {ssim.compute()}')
-        
 
         gathered_samples = [th.zeros_like(sample) for _ in range(dist.get_world_size())]
         dist.all_gather(gathered_samples, sample)  # gather not supported with NCCL
         all_images.extend([sample.cpu().numpy() for sample in gathered_samples])
+
+        gathered_orgs = [th.zeros_like(org) for _ in range(dist.get_world_size())]
+        dist.all_gather(gathered_orgs, org)  # gather not supported with NCCL
+        all_orgs.extend([org.cpu().numpy() for org in gathered_orgs])
+
+        gathered_diffs = [th.zeros_like(diff) for _ in range(dist.get_world_size())]
+        dist.all_gather(gathered_diffs, diff)  # gather not supported with NCCL
+        all_diffs.extend([diff.cpu().numpy() for diff in gathered_diffs])
+
         if args.class_cond:
             gathered_labels = [
                 th.zeros_like(classes) for _ in range(dist.get_world_size())
             ]
             dist.all_gather(gathered_labels, classes)
             all_labels.extend([labels.cpu().numpy() for labels in gathered_labels])
-        
+        logger.log(f"created {len(all_images) * args.batch_size} samples")
 
     arr = np.concatenate(all_images, axis=0)
     arr = arr[: args.num_samples]
+
+    org_arr = np.concatenate(all_orgs, axis=0)
+    org_arr = org_arr[: args.num_samples]
+
+    diff_arr = np.concatenate(all_diffs, axis=0)
+    diff_arr = diff_arr[: args.num_samples]
+
     if args.class_cond:
         label_arr = np.concatenate(all_labels, axis=0)
         label_arr = label_arr[: args.num_samples]
+        
+        name_arr = np.array(all_names)
+        name_arr = name_arr[: args.num_samples]
     
+    if dist.get_rank() == 0:
+        os.makedirs(args.result_dir, exist_ok=True)
+        shape_str = "x".join([str(x) for x in arr.shape])
+        # out_path = os.path.join(logger.get_dir(), f"samples_{shape_str}.npz")
+        out_path = os.path.join(args.result_dir, f"samples_{os.path.splitext(os.path.basename(args.model_path))[0]}_{shape_str}.npz")
+        logger.log(f"saving to {out_path}")
+        np.savez(out_path, samples=arr, labels=label_arr, names=name_arr, orgs=org_arr, diffs=diff_arr)
+        
+        final_samples_image = Image.fromarray(np.vstack(all_results))
+        logger.log(f"saving generated sample images to {args.result_dir}")
+        final_samples_image.save(os.path.join(args.result_dir, f'latest_run_{os.path.splitext(os.path.basename(args.model_path))[0]}.png'))
 
     dist.barrier()
     logger.log("sampling complete")
+    logger.log(f"psnr: {psnr.compute()}, ssim: {ssim.compute()}")
 
 
 def create_argparser():
@@ -295,7 +318,9 @@ def create_argparser():
         classifier_path="",
         classifier_scale=100,
         noise_level=500,
-        dataset='brats'
+        dataset='brats',
+        result_dir='./results',
+        use_ma_sampling=False
     )
     defaults.update(model_and_diffusion_defaults())
     defaults.update(classifier_defaults())

scripts/super_res_train.py
@@ -1,18 +1,20 @@
 """
-Train a diffusion model on images.
+Train a super-resolution model.
 """
 import sys
 import argparse
 import torch as th
 sys.path.append("..")
 sys.path.append(".")
-from guided_diffusion.bratsloader import BRATSDataset
+
+import torch.nn.functional as F
+
 from guided_diffusion import dist_util, logger
 from guided_diffusion.image_datasets import load_data
 from guided_diffusion.resample import create_named_schedule_sampler
 from guided_diffusion.script_util import (
-    model_and_diffusion_defaults,
-    create_model_and_diffusion,
+    sr_model_and_diffusion_defaults,
+    sr_create_model_and_diffusion,
     args_to_dict,
     add_dict_to_argparser,
 )
@@ -26,36 +28,27 @@ def main():
     dist_util.setup_dist()
     logger.configure()
 
-    logger.log("creating model and diffusion...")
-    model, diffusion = create_model_and_diffusion(
-        **args_to_dict(args, model_and_diffusion_defaults().keys())
+    logger.log("creating model...")
+    model, diffusion = sr_create_model_and_diffusion(
+        **args_to_dict(args, sr_model_and_diffusion_defaults().keys())
     )
     model.to(dist_util.dev())
-    schedule_sampler = create_named_schedule_sampler(args.schedule_sampler, diffusion,  maxt=1000)
+    schedule_sampler = create_named_schedule_sampler(args.schedule_sampler, diffusion)
 
     logger.log("creating data loader...")
-
-    if args.dataset == 'brats':
-        ds = BRATSDataset(args.data_dir, test_flag=False)
-        datal = th.utils.data.DataLoader(
-            ds,
-            batch_size=args.batch_size,
-            shuffle=True)
-
-    elif args.dataset == 'chexpert':
-        datal = load_data(
-            data_dir=args.data_dir,
-            batch_size=args.batch_size,
-            image_size=args.image_size,
-            class_cond=True,
-        )
-        print('dataset is chexpert')
+    data = load_superres_data(
+        args.data_dir,
+        args.batch_size,
+        large_size=args.large_size,
+        small_size=args.small_size,
+        class_cond=args.class_cond,
+    )
 
     logger.log("training...")
     TrainLoop(
         model=model,
         diffusion=diffusion,
-        data=datal,
+        data=data,
         batch_size=args.batch_size,
         microbatch=args.microbatch,
         lr=args.lr,
@@ -68,8 +61,19 @@ def main():
         schedule_sampler=schedule_sampler,
         weight_decay=args.weight_decay,
         lr_anneal_steps=args.lr_anneal_steps,
-        dataset=args.dataset
     ).run_loop()
+
+
+def load_superres_data(data_dir, batch_size, large_size, small_size, class_cond=False):
+    data = load_data(
+        data_dir=data_dir,
+        batch_size=batch_size,
+        image_size=large_size,
+        class_cond=class_cond,
+    )
+    for large_batch, model_kwargs in data:
+        model_kwargs["low_res"] = F.interpolate(large_batch, small_size, mode="area")
+        yield large_batch, model_kwargs
 
 
 def create_argparser():
@@ -80,16 +84,16 @@ def create_argparser():
         weight_decay=0.0,
         lr_anneal_steps=0,
         batch_size=1,
-        microbatch=-1,  # -1 disables microbatches
-        ema_rate="0.9999",  # comma-separated list of EMA values
-        log_interval=100,
+        microbatch=-1,
+        ema_rate="0.9999",
+        log_interval=10,
         save_interval=10000,
-        resume_checkpoint='',
+        resume_checkpoint="",
         use_fp16=False,
         fp16_scale_growth=1e-3,
-        dataset='brats'
+        result_dir='./results'
     )
-    defaults.update(model_and_diffusion_defaults())
+    defaults.update(sr_model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()
     add_dict_to_argparser(parser, defaults)
     return parser